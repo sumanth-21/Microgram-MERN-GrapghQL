be15e345de573990f4b5a9d8f7830d153528f885
sumanth-21 <saisumanthgoud.p@gmail.com>
2023-08-28 16:50:35 -0400

    developed login and registration backend logic

---
+++ server/schema/type-defs.js
@@ -0,0 +1,33 @@
+export const typeDefs = `#graphql
+    type Query {
+        loginUser(input: LoginInput): AuthPayload!
+    }
+    type User {
+        _id: ID!
+        firstName: String
+        lastName: String
+        email: String!
+        picturePath: String
+    }
+
+    type AuthPayload {
+        token: String!
+        user: User!
+    }
+
+    type Mutation {
+        registerUser(input: RegisterInput!): User!
+    }
+
+    input RegisterInput {
+        firstName: String!
+        lastName: String!
+        email: String!
+        password: String!
+        picturePath: String = ""
+    }
+    input LoginInput {
+        email: String!
+        password: String!
+    }
+`;
